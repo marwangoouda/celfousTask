7dd24994392370f63b63fb8a6a5080fd08ab6e40
marwangouda <marwangoouda@gmail.com>
2025-08-07 15:10:58 +0300

    feat(jwt-auth): Added scheduling handler and needed repo method

--- backend/src/main/java/com/celfocus/hiring/kickstarter/db/repo/CartRepository.java
+++ backend/src/main/java/com/celfocus/hiring/kickstarter/db/repo/CartRepository.java
@@ -4,11 +4,15 @@ import com.celfocus.hiring.kickstarter.db.entity.CartEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface CartRepository extends JpaRepository<CartEntity, Long> {
 
     Optional<CartEntity> findByUserId(String username);
+
+    List<CartEntity> findByLastModifiedDateBefore(LocalDateTime expirationTime);
 
     @Modifying
     void deleteByUserId(String username);
